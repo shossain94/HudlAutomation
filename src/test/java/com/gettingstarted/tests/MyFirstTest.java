1eb2437209e2b9cf048e7781c2c063cadaf80a6c
Sajjad Hossain <sajjad.sagor@live.com>
2020-07-24 15:57:53 -0400

    Adding Framework

---
+++ src/test/java/com/gettingstarted/tests/MyFirstTest.java
@@ -0,0 +1,23 @@
+package com.gettingstarted.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Title;
+
+@RunWith(SerenityRunner.class)
+public class MyFirstTest {
+	
+	@Managed
+	WebDriver driver;
+	
+	@Title("This is my first serenity test method!!!!!")
+	@Test
+	public void navigateToGooglePage() throws InterruptedException {
+		
+		
+	}
+}
