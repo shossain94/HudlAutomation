3e5e348538a667c055c27ec215eaba0aae8e5369
Sajjad Hossain <sajjad.sagor@live.com>
2020-08-29 23:54:31 -0400

    Added Reusable Methods

---
+++ src/main/java/com/plaid/utilities/ElementUtils.java
@@ -0,0 +1,36 @@
+package com.plaid.utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import net.serenitybdd.core.pages.PageObject;
+
+public class ElementUtils extends PageObject {
+
+	public ElementUtils(WebDriver driver)	{
+		super(driver);
+	}
+	
+	public boolean selectCheckBox(WebElement element)	{
+		
+		if(element.isSelected()) {
+			return true;
+		}
+		else {
+			element.click();
+			return false;
+		}
+	}
+	
+public boolean unSelectCheckBox(WebElement element)	{
+		
+		if(element.isSelected()) {
+			element.click();
+			return true;
+		}
+		else {
+			return true;
+		}
+	}
+	
+}
---
+++ src/test/java/com/automation/steps/PlaidSteps.java
@@ -0,0 +1,9 @@
+package com.automation.steps;
+
+import net.thucydides.core.steps.ScenarioSteps;
+
+public class PlaidSteps extends ScenarioSteps {
+	
+	
+
+}
